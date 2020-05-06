c41d9a06ceb61aaf712200b70990e9ecb11666d8
KogovsekZan93 <zan.kogovsek1993@gmail.com>
2020-05-06 19:25:24 +0200

    Major overhaul of the package_integrate. First fully usable version of ZIntegral1 is completed.
    The tree functions, which were used as helper functions for InterInteg.m were placed in InterInteg.m and renamed while InterInteg.m itself was renamed to ZIntegral1.m Intrafunctional documentation has been extensively updated and in places translated to English from Slovene.

--- package_integrate/TestScript.m
+++ package_integrate/TestScript.m
@@ -2,9 +2,9 @@ x=[-3;-2;3;4;5;8];
 y=sin(x);
 xmin=-2;
 xmax=4;
-n=3;
+n=2;
 
 figr=1;
-Integ = InterInteg(x,y,xmin,xmax,n,figr);
+ZInteg1 = ZIntegral1(x,y,xmin,xmax,n,figr);
 hold on;
 plot(linspace(-3,8,100),sin(linspace(-3,8,100)), 'k');
