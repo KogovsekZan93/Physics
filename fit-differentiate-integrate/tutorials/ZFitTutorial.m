09da3792d2ac466497f601abb7bc962345e34dab
KogovsekZan93 <zan.kogovsek1993@gmail.com>
2022-08-06 20:53:44 +0200

    ZFitTutorial completed
    With this commit, the tutorial for the ZFindFit function, the ZFitTutorial script, was completed.

--- fit-differentiate-integrate/tutorials/ZFitTutorial.m
+++ fit-differentiate-integrate/tutorials/ZFitTutorial.m
@@ -265,151 +265,48 @@ clearvars -except xData yData xMissing yMissingPolyFit; clc;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-% Alternatively, the assumed differentiable function which is 
-% associated with the data can be the cubic spline of the data 
-% points. This can be done by altering the optional “Type” 
-% parameter to “Spline".
-% The default “Type” parameter, which has been used until this 
-% point in the tutorial, is “A”. Note that due to spline being fully 
-% defined by a specific set of data points, there are no optional 
-% parameters “Accuracy” and “Mode” with the “Type” 
-% parameter set to “Spline”. 
-% Keep in mind that as the spline is a piecewise cubic 
-% polynomial, the order of differentiation above 3 will produce 
-% the value 0 for all inquired abscissa points. 
-% Run the following block of code to see how the spline 
-% interpolation and differentiation method compares to the
-% ZFindDerivative function with the default settings and the 
-% actual velocity function in our first case, measuring the value of 
-% the x coordinate over an 8 s interval and trying to determine 
-% the velocity function v(t). 
+% So far, the ZFitTutorial function has been used to estimate the 
+% missing values of the y variable. After that, the estimated data 
+% points along with the measured data points were plotted 
+% manually. 
+% With ZFitFunction, the function which is calculated to estimate 
+% the missing data points as well as the measured (input) data 
+% points can easily be visualized by specifying the number of 
+% the figure in which the plot is to be performed. This can be 
+% done by changing the optional “Figure” parameter from its 
+% default value of “0” to a desired natural number. 
+% Run the following block of code to visualize the data points 
+% along an example of the ZFitTutorial function calculated curve 
+% used for the missing data estimation for the example 
+% presented in this tutorial in Figure 4. 
 
-T = [0; 1; 2; 3; 4; 5; 6; 7; 8];
-X = [0; 0.841470984807897; 0.909297426825682;
-    0.141120008059867; -0.756802495307928;
-    -0.958924274663138; -0.279415498198926;
-   0.656986598718789; 0.989358246623382];
-xData = T; yData = X; 
-xDerivative = (linspace(-1, 9, 1000))';   % Time points at which 
-                                                                 % v(t) is to be evaluated. 
-yDerivativeDefault = ZFindDerivative(xData, yData, ...
-    xDerivative);   % v(t) estimation using default settings. 
-yDerivativeSpline = ZFindDerivative(xData, yData, xDerivative, ...
-    'Type', 'Spline');   % v(t) estimation using spline interpolation. 
-vActual = cos(xDerivative);   % Actual v(t). 
-close all; figure(1); hold on;
-plot(xDerivative, vActual, 'k', 'LineWidth', 1.5);
-plot(xDerivative, yDerivativeDefault, 'b', 'LineWidth', 1.5);
-plot(xDerivative, yDerivativeSpline, 'r', 'LineWidth', 1.5);
-xlabel('t [s]'); ylabel('v [m / s]'); set(gca, 'FontSize', 14); grid on;
-legend('Actual', 'Estimated using default settings', ...
-    'Estimated using the spline interpolation');
-clearvars; clc;
+xData = [0; 1; 3; 5; 8; 35; 37; 40; 45; 56; 57; 60; 66; 68];
+yData = [10; 9; 8; 11; 13; 5; 4; 3; 2; 6; 11; 12; 2; 8];
+xMissing = [-5; -4; -3; -2; -1; 2; 4; 6; 7; 9; 10; 11; 12; 13; 14; 15; ...
+    16; 17; 18; 19; 20; 21; 22; 23; 24; 25; 26; 27; 28; 29; 30; 31; ...
+    32; 33; 34; 36; 38; 39; 41; 42; 43; 44; 46; 47; 48; 49; 50; 51; ...
+    52; 53; 54; 55; 58; 59; 61; 62; 63; 64; 65; 67; 69; ...
+    70];   % The missing integer values of the x variable at which 
+              % the y variable was not measured. 
+close all;
+psacc = 3;   % Pseudo Accuracy paramater 
+                     % set to 3. 
+mode = 2;   % Mode paramater set to 2. 
+figr = 4;
+yMissing = ZFindFit(xData, yData, xMissing, ...
+    'PseudoAccuracy', psacc, 'Mode', mode, ...
+    'Figure', figr);   % Figure paramater set to 4. 
+clearvars -except xData yData xMissing yMissing; clc;
 
 
 %                                           Page 7
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-% Another way to find the numerical derivative of a function is to 
-% differentiate the regression polynomial of the data points. This 
-% can be done by altering the optional “Type” parameter to 
-% “PolyFit”. In that case, the required “PolyDegree” parameter 
-% needs to be set. The “PolyDegree” parameter corresponds to 
-% the degree of the regression polynomial. 
-% As with the “Spline” setting of the “Type” parameter, there are 
-% no optional parameters “Pseudo Accuracy” and “Mode”. 
-% We again refer to the first case, measuring the value of 
-% the x coordinate over an 8 s interval and trying to determine 
-% the velocity function v(t). Run the block of code on Page 1 to 
-% see the data points again. With no additional information, the 
-% most reasonable regression polynomial would likely be that of 
-% the fourth degree as the x(t) function seems to have three 
-% local extremes. 
-% Run the following block of code to see how the “PolyFit” 
-% setting of the “Type” parameter compares to spline 
-% interpolation and differentiation, the ZFindDerivative function 
-% with the default settings, and the actual velocity function. 
-
-T = [0; 1; 2; 3; 4; 5; 6; 7; 8];
-X = [0; 0.841470984807897; 0.909297426825682;
-    0.141120008059867; -0.756802495307928;
-    -0.958924274663138; -0.279415498198926;
-   0.656986598718789; 0.989358246623382];
-xData = T; yData = X; 
-xDerivative = (linspace(-1, 9, 1000))';   % Time points at which 
-                                                                 % v(t) is to be evaluated. 
-yDerivativeDefault = ZFindDerivative(xData, yData, ...
-    xDerivative);   % v(t) estimation using default settings. 
-yDerivativeSpline = ZFindDerivative(xData, yData, xDerivative, ...
-    'Type', 'Spline');   % v(t) estimation using spline interpolation. 
-polyDegree = 4;   % Degree of the regression polynomial. 
-yDerivativePolyFit = ZFindDerivative(xData, yData, xDerivative, ...
-    'Type', 'PolyFit', ...
-    polyDegree);   % v(t) estimation using the regression 
-                              % polynomial. 
-vActual = cos(xDerivative);   % Actual v(t). 
-close all; figure(1); hold on;
-plot(xDerivative, vActual, 'k', 'LineWidth', 1.5);
-plot(xDerivative, yDerivativeDefault, 'b', 'LineWidth', 1.5);
-plot(xDerivative, yDerivativeSpline, 'r', 'LineWidth', 1.5);
-plot(xDerivative, yDerivativePolyFit, 'g', 'LineWidth', 1.5);
-xlabel('t [s]'); ylabel('v [m / s]'); set(gca, 'FontSize', 14); grid on;
-legend('Actual', 'Estimated using default settings', ...
-    'Estimated using the spline interpolation', ...
-    'Estimated using the regression polynomial');
-clearvars; clc;
-
-
-%                                           Page 8
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-% At times it may be important to visualize the function which is 
-% differentiated to calculate the numerical derivative. In such 
-% cases, the optional “Figure” parameter can be set to the figure 
-% index in which the function is to be visualized. Setting the 
-% “Figure” parameter to “0” results in no visualization. 
-% Run the following block of code to visualize the data points 
-% along with the functions which were differentiated for each of 
-% the numerical derivatives calculated on the previous page. 
-
-T = [0; 1; 2; 3; 4; 5; 6; 7; 8];
-X = [0; 0.841470984807897; 0.909297426825682;
-    0.141120008059867; -0.756802495307928;
-    -0.958924274663138; -0.279415498198926;
-   0.656986598718789; 0.989358246623382];
-xData = T; yData = X; 
-xDerivative = (linspace(-1, 9, 1000))';   % Time points at which 
-                                                                 % v(t) is to be evaluated. 
-close all; hold on;
-figr1 = 1;   % Figure paramater value 
-yDerivativeDefault = ZFindDerivative(xData, yData, xDerivative, ...
-    'Figure', figr1);   % v(t) estimation using default settings. 
-title('Default settings of ZFindDerivative function');
-figr2 = 2;   % Figure paramater value 
-yDerivativeSpline = ZFindDerivative(xData, yData, xDerivative, ...
-    'Type', 'Spline', ...
-    'Figure', figr2);   % v(t) estimation using spline interpolation.
-title('Spline interpolation');
-polyDegree = 4;   % Degree of the regression polynomial. 
-figr3 = 3;   % Figure paramater value 
-yDerivativePolyFit = ZFindDerivative(xData, yData, xDerivative, ...
-    'Type', 'PolyFit', polyDegree, ...
-    'Figure', figr3);   % v(t) estimation using the regression 
-                                % polynomial. 
-title('Regression polynomial');
-clearvars; clc;
-
-
-%                                           Page 9
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-% This completes the tutorial for the ZFindDerivative function. 
+% This completes the tutorial for the ZFindFit function. 
 % For further questions, the documentation and the code of the 
 % function and its subfunctions should be referred to. 
 
 
-%                                           Page 10
+%                                           Page 8
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
