8f0ddc1824d95cf34ce8eb2f940deb5d97390529
KogovsekZan93 <zan.kogovsek1993@gmail.com>
2022-11-26 21:28:51 +0100

    With this commit, the descriptions of the variables for the functions ZFind…SplineBasic were written.
    With this commit, the descriptions of the variables for the functions ZFind…SplineBasic were written.

--- fit-differentiate-integrate/subfunctions/subsubfunctions/ZFindDerivativeSplineBasic.m
+++ fit-differentiate-integrate/subfunctions/subsubfunctions/ZFindDerivativeSplineBasic.m
@@ -1,22 +1,88 @@
-function [yDerivativeSpline, varargout] = ZFindDerivativeSplineBasic(xData, yData, xDerivativeSpline, varargin)
+function [yDerivativeSpline, varargout] = ...
+    ZFindDerivativeSplineBasic...
+    (xData, yData, xDerivativeSpline, varargin)
+%% Numerical spline-based differentiation tool
+% 
+% Author: Žan Kogovšek
+% Date: 11.26.2022
+% Last changed: 11.26.2022
+% 
+%% Description
+% 
+% Given the input vector "xData" of the independent variable X 
+% and the input vector "yData" of the values of the dependent 
+% variable Y of an arbitrary function Y = f(X), this function returns 
+% the vector "yDerivativeSpline" of the estimated values of 
+% f^("OrdDeriv")("xDerivativeSpline"), where f^("OrdDeriv") is the 
+% "OrdDeriv"-th order derivative of the f function and 
+% "xDerivativeSpline" is the input vector of values of the X 
+% variable. The estimation is based on the spline interpolation of 
+% the data points represented by the pairs ("xData"(i), "yData"(i)). 
+% The optional parameter "OrdDeriv" can be used to set the 
+% order of differentiation, the default value of which is 
+% "OrdDeriv" == 1. 
+% 
+%% Variables
+% 
+% This function has the form of [yDerivativeSpline, varargout] = ...
+% ZFindDerivativeSplineBasic...
+% (xData, yData, xDerivativeSpline, varargin)
+% 
+% "xData" and "yData" are the vectors of the values of the 
+% independent variable X and of the dependent variable Y, 
+% respectively, of an arbitrary function Y = f(X) 
+% ("yData" = f("xData")). 
+% Both the "xData" vector and the "yData" vector must be 
+% column vectors of equal length and of real numbers. The 
+% values of the "xData" vector must be in ascending order. 
+% 
+% "xDerivativeSpline" is the vector of the values of the 
+% independent variable X at which the value of the derivative of 
+% the f function is to be estimated. The "xDerivativeSpline" 
+% vector must be a column vector of real numbers. 
+% 
+% "varargin" represents the optional input parameter "OrdDeriv". 
+% "OrdDeriv" is the name of the parameter the value of which is 
+% the derivative order. It must be a natural number. The default 
+% value is "1". 
+% 
+% "yDerivativeSpline" is the column vector of the estimated 
+% values of f^("OrdDeriv")("xDerivativeSpline"). 
+% 
+% "varargout" represents the optional output parameters 
+% "ppDerivativeSpline" and "ppFitSpline".
+%     "ppDerivativeSpline" is the piecewise polynomial which is 
+%     the estimation of the function f^("OrdDeriv"). It can be 
+%     evaluated by the MATLAB ppval function. The details of the 
+%     "ppDerivativeSpline" piecewise polynomial can be 
+%     extracted by the MATLAB unmkpp function. 
+%     "ppFitSpline" is the cubic spline of the data points 
+%     represented by the pairs ("xData"(i), "yData"(i)) which is the 
+%     estimation of the f function. It can be evaluated by the 
+%     MATLAB ppval function. The details of the " ppFitSpline" 
+%     piecewise polynomial can be extracted by the MATLAB 
+%     unmkpp function. 
 
 
 pars = inputParser;
 
 paramName = 'xData';
-errorMsg = '''xData'' must be a sorted column vector of numbers.';
+errorMsg = ...
+    '''xData'' must be a sorted column vector of numbers.';
 validationFcn = @(x)assert(isnumeric(x) && iscolumn(x) && ... 
     issorted(x), errorMsg);
 addRequired(pars, paramName, validationFcn);
 
 paramName = 'yData';
-errorMsg = '''yData'' must be a column vector of numbers which has the same length as ''xData''';
+errorMsg = ...
+    '''yData'' must be a column vector of numbers which has the same length as ''xData''';
 validationFcn = @(x)assert(isnumeric(x) && iscolumn(x) &&  ...
     length(xData) == length(yData), errorMsg);
 addRequired(pars, paramName, validationFcn);
 
 paramName = 'xDerivativeSpline';
-errorMsg = '''xDerivativeSpline'' must be a sorted column vector of numbers.';
+errorMsg = ...
+    '''xDerivativeSpline'' must be a sorted column vector of numbers.';
 validationFcn = @(x)assert(isnumeric(x) && iscolumn(x) && ... 
     issorted(x), errorMsg);
 addRequired(pars, paramName, validationFcn);
@@ -32,7 +98,6 @@ parse(pars, xData, yData, xDerivativeSpline, varargin{:});
 
 ordDeriv = pars.Results.OrdDeriv;
 
-
 pp = spline(xData, yData);
 [breaks, coefs, ~, ~, ~] = unmkpp(pp);
 ppFitSpline = mkpp(breaks, coefs);
@@ -42,7 +107,8 @@ coefsDeriv = coefs;
 if ordDeriv < 4
     for i = 1 : ordDeriv
         ordPoly = length(coefsDeriv(1, :)) - 1;
-        coefsDeriv = times(coefsDeriv(:, 1 : ordPoly), repmat((ordPoly : -1 : 1), length(coefsDeriv(:,1)), 1));
+        coefsDeriv = times(coefsDeriv(:, 1 : ordPoly), ...
+            repmat((ordPoly : -1 : 1), length(coefsDeriv(:,1)), 1));
     end
 else
      coefsDeriv = zeros(length(coefsDeriv(:,1)),1);
--- fit-differentiate-integrate/subfunctions/subsubfunctions/ZFindIntegralSplineBasic.m
+++ fit-differentiate-integrate/subfunctions/subsubfunctions/ZFindIntegralSplineBasic.m
@@ -1,34 +1,86 @@
-function [yIntegralSpline, varargout] = ZFindIntegralSplineBasic(xData, yData, xIntegralSpline)
+function [yIntegralSpline, varargout] = ZFindIntegralSplineBasic...
+    (xData, yData, xIntegralSpline)
+%% Numerical spline-based indefinite integration tool
+% 
+% Author: Žan Kogovšek
+% Date: 11.26.2022
+% Last changed: 11.26.2022
+% 
+%% Description
+% 
+% Given the input vector "xData" of the independent variable X 
+% and the input vector "yData" of the values of the dependent 
+% variable Y of an arbitrary function Y = (df/dX)(X), this function 
+% returns the vector "yIntegralSpline" of the estimated values of 
+% f("xIntegralSpline") - f("xIntegralSpline"(1)), where 
+% "xIntegralSpline" is the input vector of values of the X variable. 
+% The estimation is based on the spline interpolation of the data 
+% points represented by the pairs ("xData"(i), "yData"(i)). 
+% 
+%% Variables
+% 
+% This function has the form of [yIntegralSpline, varargout] = ...
+% ZFindIntegralSplineBasic(xData, yData, xIntegralSpline)
+% 
+% "xData" and "yData" are the vectors of the values of the 
+% independent variable X and of the dependent variable Y, 
+% respectively, of an arbitrary function Y = (df/dX)(X) 
+% ("yData" = (df/dX) ("xData")). 
+% Both the “xData” vector and the “yData” vector must be 
+% column vectors of equal length and of real numbers. The 
+% values of the "xData" vector must be in ascending order. 
+% 
+% "xIntegralSpline" is the vector of the values of the independent 
+% variable X at which the values of the vector 
+% f("xIntegralSpline") - f("xIntegralSpline"(1)) is to be estimated. 
+% The "xIntegralSpline" vector must be a column vector of real 
+% numbers. The values of the "xIntegralSpline" vector must be 
+% in ascending order. 
+% 
+% "yIntegralSpline" is the column vector of the estimated values 
+% of f("xIntegralSpline") - f("xIntegralSpline"(1)). 
+% 
+% "varargout" represents the optional output parameter 
+% "ppFitSpline", which is the cubic spline of the data points 
+% represented by the pairs ("xData"(i), "yData"(i)) which is the 
+% estimation of the function df/dX. It can be evaluated by the 
+% MATLAB ppval function. The details of the " ppFitSpline" 
+% piecewise polynomial can be extracted by the MATLAB 
+% unmkpp function. 
 
 
 pars = inputParser;
 
 paramName = 'xData';
-errorMsg = '''xData'' must be a sorted column vector of numbers.';
+errorMsg = ...
+    '''xData'' must be a sorted column vector of numbers.';
 validationFcn = @(x)assert(isnumeric(x) && iscolumn(x) && ... 
     issorted(x), errorMsg);
 addRequired(pars, paramName, validationFcn);
 
 paramName = 'yData';
-errorMsg = '''yData'' must be a column vector of numbers which has the same length as ''xData''';
+errorMsg = ...
+    '''yData'' must be a column vector of numbers which has the same length as ''xData''';
 validationFcn = @(x)assert(isnumeric(x) && iscolumn(x) &&  ...
     length(xData) == length(yData), errorMsg);
 addRequired(pars, paramName, validationFcn);
 
 paramName = 'xIntegralSpline';
-errorMsg = '''xIntegralSpline'' must be a sorted column vector of numbers.';
+errorMsg = ...
+    '''xIntegralSpline'' must be a sorted column vector of numbers.';
 validationFcn = @(x)assert(isnumeric(x) && iscolumn(x) && ... 
     issorted(x), errorMsg);
 addRequired(pars, paramName, validationFcn);
 
 parse(pars, xData, yData, xIntegralSpline);
 
-
 pp = spline(xData, yData);
 [breaks, coefs, ~, ~, ~] = unmkpp(pp);
 ppFitSpline = mkpp(breaks, coefs);
 coefsLength = length(coefs);
-coefsInteg = [times(coefs, repmat([1/4, 1/3, 1/2, 1], coefsLength, 1)), zeros(coefsLength,1)];
+coefsInteg = ...
+    [times(coefs, repmat([1/4, 1/3, 1/2, 1], coefsLength, 1)), ...
+    zeros(coefsLength,1)];
 
 breaksReal = breaks;
 breaksReal(1) = - inf; 
@@ -44,28 +96,38 @@ while breaksReal(j) <= xIntegralSpline(1)
     j = j +1;
 end
 
-ppIntegralSpline = mkpp([breaks(j - 1); breaks(j)], coefsInteg(j - 1, :));
+ppIntegralSpline = mkpp...
+    ([breaks(j - 1); breaks(j)], coefsInteg(j - 1, :));
 
 Summa = 0;
 
 for b = 2 : xIntegralSplineLength
     if xIntegralSpline(b) >= breaksReal(j)
-        yIntegralSpline(a : b - 1) = Summa + ppval(ppIntegralSpline, xIntegralSpline(a : b - 1)) - ppval(ppIntegralSpline, xIntegralSpline(a));
-        Summa = yIntegralSpline(b - 1) + ppval(ppIntegralSpline, breaks(j)) - ppval(ppIntegralSpline, xIntegralSpline(b - 1));
+        yIntegralSpline(a : b - 1) = Summa + ...
+            ppval(ppIntegralSpline, xIntegralSpline(a : b - 1)) - ...
+            ppval(ppIntegralSpline, xIntegralSpline(a));
+        Summa = yIntegralSpline(b - 1) + ...
+            ppval(ppIntegralSpline, breaks(j)) - ...
+            ppval(ppIntegralSpline, xIntegralSpline(b - 1));
         j = j + 1;
         while breaksReal(j) <= xIntegralSpline(b)
-            ppIntegralSpline = mkpp([breaks(j - 1); breaks(j)], coefsInteg(j - 1, :));
-            Summa = Summa + ppval(ppIntegralSpline, breaks(j)) - ppval(ppIntegralSpline, breaks(j - 1));
+            ppIntegralSpline = mkpp...
+                ([breaks(j - 1); breaks(j)], coefsInteg(j - 1, :));
+            Summa = Summa + ppval(ppIntegralSpline, breaks(j)) - ...
+                ppval(ppIntegralSpline, breaks(j - 1));
             j = j +1;
         end
         a = b;
-        ppIntegralSpline = mkpp([breaks(j - 1); breaks(j)], coefsInteg(j - 1, :));
+        ppIntegralSpline = mkpp...
+            ([breaks(j - 1); breaks(j)], coefsInteg(j - 1, :));
     end
 end
 
-yIntegralSpline(a : end) = Summa + ppval(ppIntegralSpline, xIntegralSpline(a : end)) - ppval(ppIntegralSpline, max(breaks(j - 1), xIntegralSpline(max(a - 1, 1))));
+yIntegralSpline(a : end) = Summa + ppval...
+    (ppIntegralSpline, xIntegralSpline(a : end)) - ppval...
+    (ppIntegralSpline, max(breaks(j - 1), ...
+    xIntegralSpline(max(a - 1, 1))));
 
 varargout = {ppFitSpline};
 
 end
-
